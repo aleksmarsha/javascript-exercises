8f7937e39b49fa00b4e95193529064e63f5242a4
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-21 04:54:26 +0200

    Make the second javascript tdd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    let sumOfString = '';
+    if (number < 0) {
+        return 'ERROR';
+    } else if (number === 0 || string === '') {
+        return sumOfString;
+    } else {
+        for (let i = 0; i < number; i++) {
+            sumOfString += string;
+        }
+        return sumOfString;
+    }
 };
 
 // Do not edit below this line
