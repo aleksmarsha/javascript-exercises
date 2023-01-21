c5588d6c136267c4d42acb9520409d1c6bb6e47e
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-21 05:18:45 +0200

    Make the third javascript tdd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let completeString = '';
+    let lengthString = string.length;
+    for (let i = 1; i <= lengthString; i++) {
+        completeString += string.charAt(lengthString - i);
+    }
+    return completeString;
 };
 
 // Do not edit below this line
