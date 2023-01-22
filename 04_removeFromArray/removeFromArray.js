a141a0e172d534f7f90a3d8f4e7ea0f7d5d85f8c
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-22 05:43:19 +0200

    Make the fourth javascript tdd exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(mainArray, ...notNeedInArray) {
+    for (let needless of notNeedInArray) {
+            if (mainArray.includes(needless)) {
+                let x = mainArray.indexOf(needless);
+                mainArray.splice(x, 1);
+            }
+    }
+    return mainArray;
 };
 
 // Do not edit below this line
