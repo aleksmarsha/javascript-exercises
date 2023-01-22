94932eb1eaee5073ec8d83bf97473bcf8eb79a94
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-23 01:44:11 +0200

    Make the fifth javascript tdd exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let result = 0;
+    let big;
+    let i;
+    if ((typeof firstNumber === typeof secondNumber) && (firstNumber >= 0 && secondNumber >= 0)) {
+        if (firstNumber > secondNumber) {
+            big = firstNumber;
+            i = secondNumber;
+        } else {
+            big = secondNumber;
+            i = firstNumber;
+        }
 
+        for (; i <= big; i++) {
+            result += i;
+        }
+
+        return result;
+    } else {
+        return `ERROR`;
+    }
 };
 
 // Do not edit below this line
