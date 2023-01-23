b08d4c80e02cac1fb7af4fd78bbab3129f4bfc3d
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-01-23 05:01:28 +0200

    Make the seventh javascript tdd exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(oneTemperature) {
+  let celsius = (oneTemperature - 32) * 0.5556;
+  return Math.round(celsius * 10) / 10; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(twoTemperature) {
+  let fahrenheit = (twoTemperature * 1.8) + 32;
+  return Math.round(fahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
