993be56df8189910ead59b4894ae961c110d78af
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-03-04 17:08:47 +0200

    Make the twelveth javascript tdd exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arrayx) {
+	arrayx.sort((one, two ) => {
+		if(one.yearOfDeath && two.yearOfDeath){
+		const firstPerson = one.yearOfDeath - one.yearOfBirth;
+		const secondPerson = two.yearOfDeath - two.yearOfBirth;
+		return secondPerson - firstPerson;
+		} else {
+			let personOne;
+			let personTwo;
+			let full = new Date();
+			if(!one.yearOfDeath) {
+				personOne = full.getFullYear() - one.yearOfBirth;
+				personTwo = two.yearOfDeath - two.yearOfBirth; 
+			} else {
+				personOne = one.yearOfDeath - one.yearOfBirth;
+				personTwo = full.getFullYear() - two.yearOfBirth;
+			}
+			return personTwo - personOne;
+		}
+	});
+	return arrayx[0];
 };
 
 // Do not edit below this line
