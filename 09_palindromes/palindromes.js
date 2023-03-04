99ee7573c6b906b9ccc39ca712220699aa686e94
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-03-04 04:01:31 +0200

    Make the ninth javascript tdd exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (miss) {
+	const first = Array.from(miss);
+	let alma = [];
+	let telhma = [];
+	first.forEach(ltr => {
+		if (ltr.toLowerCase() !== ltr.toUpperCase()) {
+			alma.push(ltr.toLowerCase());
+			telhma.unshift(ltr.toLowerCase());
+		}
+	});
+	if (alma.toString() === telhma.toString()) {
+		return true;
+	} else {
+		return false;
+	}
 };
 
 // Do not edit below this line
