ba0e42ad70f36c09c63f2407130a30366ada4e7b
aleksmarsha <27923826+aleksmarsha@users.noreply.github.com>
2023-03-04 04:55:16 +0200

    Make the tenth javascript tdd exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(bona) {
+	const numbers = {};
+	let firstIndex = 1;
+	let secondIndex = 1;
+	numbers[1] = firstIndex;
+	numbers[2] = secondIndex;
+	for (let i = 3; i < 37; i++) {
+		firstIndex += secondIndex;
+        secondIndex = firstIndex - secondIndex;
+        numbers[i] = firstIndex;
+	}
+	if(parseInt(bona) <= 0) {
+		return 'OOPS';
+	} else {
+		return numbers[bona];
+	}
 };
 
 // Do not edit below this line
